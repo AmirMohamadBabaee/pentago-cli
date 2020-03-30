fe077c23107000029eccef36367c07f0b066903f
9831011 <9831011@aut.ac.ir>
2020-03-30 21:50:52 +0430

    Add fields & constructor & makeMap and indexConverter method

--- src/Table.java
+++ src/Table.java
@@ -1,3 +1,5 @@
+import jdk.nashorn.internal.objects.annotations.Constructor;
+
 /**
  * Table
  *
@@ -9,4 +11,122 @@
  * @version v1.0 (30 Mar 2020)
  */
 public class Table {
+
+    // Fields
+
+    // First block
+    private Block block1;
+    // Second block
+    private Block block2;
+    // Third block
+    private Block block3;
+    // Fourth block
+    private Block block4;
+    // whole table of this game
+    private int[][] map;
+
+
+    // Constructor
+
+    // TODO: add java doc for this constructor
+    public Table() {
+
+        this.block1 = new Block();
+        this.block2 = new Block();
+        this.block3 = new Block();
+        this.block4 = new Block();
+        this.map = new int[6][6];
+    }
+
+
+    // Setter & Getter
+
+    /**
+     * getter of map 2D array is 6*6
+     *
+     * @return 2D array called map
+     */
+    public int[][] getMap() {
+        return map;
+    }
+
+    // Method
+
+    /**
+     * this method combine 4 block in this table map
+     */
+    public void makeMap() {
+
+        for(int i=0 ; i<6 ; i++) {
+            for(int j=0 ; j<6 ; j++) {
+
+                if(i<3) {
+
+                    if(j<3) {
+                        this.map[i][j] = block1.getBlock()[i][j];
+                    } else {
+                        this.map[i][j] = block2.getBlock()[i][j-3];
+                    }
+
+                } else {
+
+                    if(j<3) {
+                        this.map[i][j] = block3.getBlock()[i-3][j];
+                    } else {
+                        this.map[i][j] = block4.getBlock()[i-3][j-3];
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    /**
+     * this method take x and y position in map array and
+     * convert them to x and y of specific block and call
+     * that block itself
+     *
+     * @param color color of marble
+     * @param mapX x position in map 2D array
+     * @param mapY y position in map 2D array
+     */
+    private void indexConverter(int color ,int mapX, int mapY) {
+
+        if(mapX >= 0 && mapX < 6 && mapY >= 0 && mapY < 6) {
+
+            if(mapX < 3) {
+
+                if(mapY < 3) { // first block (1)
+
+                    block1.addMarble(color, mapX, mapY);
+
+                } else { // second block (2)
+
+                    block2.addMarble(color, mapX, mapY-3);
+
+                }
+
+            } else {
+
+                if(mapY < 3) { // third block (3)
+
+                    block3.addMarble(color, mapX-3, mapY);
+
+                } else { // forth block (4)
+
+                    block4.addMarble(color, mapX, mapY);
+
+                }
+
+            }
+
+        } else {
+            System.out.println("entered index is not correct!!!");
+        }
+
+    }
+
+
+    public 
 }
