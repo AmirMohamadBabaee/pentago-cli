26674824826edff331718b969c4d738c9a36c6f7
9831011 <9831011@aut.ac.ir>
2020-03-30 20:52:02 +0430

    Create Table class

---
+++ src/Table.java
@@ -0,0 +1,12 @@
+/**
+ * Table
+ *
+ * This class collect four block and use them
+ * together. also draw table method is in this
+ * class
+ *
+ * @author Amir01
+ * @version v1.0 (30 Mar 2020)
+ */
+public class Table {
+}
