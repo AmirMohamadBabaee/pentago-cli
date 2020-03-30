cf3b8fb77cc1af7ed7971f38dfcb160ea6e10134
9831011 <9831011@aut.ac.ir>
2020-03-30 19:07:56 +0430

    Create Block class

---
+++ src/Block.java
@@ -0,0 +1,12 @@
+/**
+ * Block
+ *
+ * This class define blocks of pentago game that can
+ * rotate clockwise or counter clockwise
+ * This class can accept at most 9 marble in 3*3 square
+ *
+ * @author Amir01
+ * @version v1.0 (30 Mar 2020)
+ */
+public class Block {
+}
