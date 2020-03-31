d5a1a680694a6d055722987b62bd4149301e34fd
9831011 <9831011@aut.ac.ir>
2020-03-31 20:53:20 +0430

    Create Condition class

---
+++ src/Condition.java
@@ -0,0 +1,11 @@
+/**
+ * Condition
+ *
+ * This class have behaviors to check end of game
+ * and can check a marble position is valid or not
+ *
+ * @author Amir01
+ * @version v1.0 (31 Mar 2020)
+ */
+public class Condition {
+}
